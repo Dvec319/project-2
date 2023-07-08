a2c5f8215eac5e589c4872502da63dc89cdd92ab
mdelvecchio <mdelvecchio3119@gmail.com>
2023-07-08 13:23:33 -0400

    Index page is up and running with some css put together

---
+++ models/videogame.js
@@ -0,0 +1,13 @@
+const mongoose = require('./connection');
+
+const gameSchema = new mongoose.Schema({
+    name: String,
+    publisher: String,
+    yearReleased: Number,
+    image: String,
+    briefDescription: String
+});
+
+const Game = mongoose.model('game', gameSchema);
+
+module.exports = Game
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config(); // bring in our .enc vars
 const express = require('express'); // web framework for node
 const morgan = require('morgan'); // logger for node
 const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
+const Game = require('./models/videogame')
 
 // express application
 const app = express();
@@ -21,8 +22,10 @@ app.use(express.static('public')); // serve static files from public folder
 ///////////////////////
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.get('/', async (req, res) => {
+    const allGames = await Game.find({})
+
+    res.render('games/index.ejs', {games: allGames})
 });
 
 ///////////////////////////
